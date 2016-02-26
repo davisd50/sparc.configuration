a140a6ffdaa764d55da030b3dab91719014d9809
David Davis <davisd50@gmail.com>
2016-02-26 08:52:30 -0500

    configure to insure XMLTree objects implements markers

--- sparc/configuration/xml/interfaces.py
+++ sparc/configuration/xml/interfaces.py
@@ -3,5 +3,8 @@ from zope.interface import Interface
 class IElementTree(Interface):
     """Marker for an ElementTree instance"""
 
+class IXMLElement(Interface):
+    """Marker for an ElementTree Element instance"""
+
 class IAppElementTreeConfig(IElementTree):
     """Marker for a ElementTree based application configuration instance"""
